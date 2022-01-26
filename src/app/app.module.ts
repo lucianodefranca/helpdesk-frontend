6a87819199a3ae84c6b294297be80c51ca369af6
luciano de franca <lucianodefranca86@gmail.com>
2022-01-25 22:39:13 -0300

    Componente footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './views/components/template/header/header.component';
 
 import {MatToolbarModule} from '@angular/material/toolbar';
+import { FooterComponent } from './views/components/template/footer/footer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
